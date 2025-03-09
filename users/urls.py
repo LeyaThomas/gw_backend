be7f76d33247a3f46da9e61402e73b2123573c7e
LeyaThomas <leyathomas1352002@gmail.com>
2025-03-09 23:09:20 +0530

    feat(users):users-model-auth-logic-added

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import RegisterView, LoginView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path('signup/', RegisterView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import status, generics
 from rest_framework.permissions import AllowAny
-from django.contrib.auth import login
 from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import CustomUserSerializer, LoginSerializer
 from django.contrib.auth import get_user_model
@@ -13,15 +12,23 @@ class RegisterView(generics.CreateAPIView):
     permission_classes = [AllowAny]
     serializer_class = CustomUserSerializer
 
-    def perform_create(self, serializer):
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        # Generate JWT token upon successful registration
+
+        # Generate JWT tokens upon successful registration
         refresh = RefreshToken.for_user(user)
         access_token = str(refresh.access_token)
+
         return Response({
-            'message': 'Registration successful',
-            'access_token': access_token,
-            'refresh_token': str(refresh),
+            "message": "Registration successful",
+            "token": {
+                "access": access_token,
+                "refresh": str(refresh)
+            },
+            "role": user.role,
+            "username": user.username
         }, status=status.HTTP_201_CREATED)
 
 
@@ -32,7 +39,7 @@ class LoginView(generics.GenericAPIView):
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
+        user = serializer.validated_data["user"]
 
         # Generate JWT token
         refresh = RefreshToken.for_user(user)
@@ -44,5 +51,6 @@ class LoginView(generics.GenericAPIView):
                 "access": access_token,
                 "refresh": str(refresh)
             },
-            "role": user.role
+            "role": user.role,
+            "username": user.username,
         }, status=status.HTTP_200_OK)
