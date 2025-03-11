96cc7b1a2e890f016e42b64155e72b705b213118
LeyaThomas <leyathomas1352002@gmail.com>
2025-03-11 21:13:49 +0530

    requirement file added

--- guarded_backend/settings.py
+++ guarded_backend/settings.py
@@ -62,7 +62,10 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-    ]
+    ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),  
